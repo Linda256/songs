858233256835f8f9d21b15ba7f7887337e24eaed
Linda Wang <lindawang256@gmail.com>
2019-03-05 17:47:02 -0800

    Add sample data

---
+++ src/SampleData.js
@@ -0,0 +1,17 @@
+const SampleSongs=[
+    {
+        Title: "Love you forever",
+        Length:"2:57"
+    },
+    {
+        Title: "Raining days",
+        Length:"3:17"
+    },
+    {
+        Title: "Rose, rose,I love you",
+        Length:"4:08"
+    },
+    
+]
+
+export default SampleSongs;
